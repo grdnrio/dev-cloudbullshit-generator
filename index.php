ab1b071a193c8cfa0f3cd5b3683d2784c1192a1b
Joe Gardiner <joe@joegardiner.co.uk>
2012-09-18 23:01:38 +0200

    Imported google font, responsive stylesheet, title and menu working.

--- index.php
+++ index.php
@@ -2,15 +2,30 @@
     <head>
     	<title>Cloud Bullshit Generator</title>
     	<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js'></script>
-	<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
-	<link href='http://fonts.googleapis.com/css?family=McLaren' rel='stylesheet' type='text/css'>
+    	<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+    	<link href='http://fonts.googleapis.com/css?family=McLaren' rel='stylesheet' type='text/css'>
     </head>
     <body>
-    	<p><?php include("topMenu.php"); ?></p>
-
-<div id="bullshit">
- <?php include("selectContent.php"); ?>
-</div>
-<div class="divCenter" id="buttonMore"> 
-<input type="button" value="More Bullshit" onClick="window.location.reload()">
-</div>
+    	<div id="header">
+    		<div id="title">
+    			<p>Cloud</p>
+    			<p>Bullshit</p>
+    			<p>Generator</p>
+    		</div>
+    		<div id="menu">
+    			<ul>
+					<li class="left"><a href="#home">Home</a></li>
+					<li class="right"><a href="#signup">Signup</a></li>
+					<li class="left"><a href="#login">Login</a></li>
+					<li class="right"><a href="#about">About</a></li>
+				</ul>
+			</div>
+    	</div>
+    	<div id="bullshit">
+	    	<?php include("selectContent.php"); ?>
+	    </div>
+	    <div class="divCenter" id="buttonMore"> 
+		    <input type="button" value="More Bullshit" onClick="window.location.reload()">
+		</div>
+    </body>
+</html>
--- topMenu.php
+++ topMenu.php
@@ -1,8 +1,3 @@
-<html>
-<body>
-<a href="#">Home</a> - 
-<a href="#">Register</a> - 
-<a href="#">Login</a> - 
-<a href="#">About</a> <br />
-</body>
-</html>
+		
+
+
